87feb0733f8139b3fb7b464d9630c8514181084d
DenisEgorov200 <egorov.denis.mem@gmail.com>
2023-10-07 15:39:24 +0300

    feat: add main font

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,3 @@
 export const App = () => {
-  return <h1>MORENT</h1>;
+  return <h1>hello</h1>;
 };
--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,9 @@
 export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
+    fontFamily: {
+      sans: ['Plus Jakarta Sans', 'sans-serif'],
+    },
     colors: {
       primary: {
         100: '#D6E4FD',
