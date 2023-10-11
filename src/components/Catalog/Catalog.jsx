b138565ef89416cb60078718270261d7e56dc25b
DenisEgorov200 <egorov.denis.mem@gmail.com>
2023-10-11 22:05:51 +0300

    feat(Catalog): add show more button

--- src/components/Catalog/Catalog.jsx
+++ src/components/Catalog/Catalog.jsx
@@ -8,6 +8,11 @@ import { PrimaryButton } from '../ui/PrimaryButton.jsx';
 
 export const Catalog = () => {
   const [carDesc, setCardDesc] = useState([]);
+  const [count, setCount] = useState(8);
+
+  const handleShowMore = () => {
+    setCount(count + 4);
+  };
 
   useEffect(() => {
     getCarDesc().then((data) => setCardDesc(data));
@@ -23,14 +28,16 @@ export const Catalog = () => {
         {carDesc.map((desc) => <CatalogCard key={uuidv4()} desc={desc} />).splice(0, 4)}
       </div>
       <div className="flex items-center justify-between mb-5">
-        <h4 className="font-semibold text-secondary-300">Recomendation car</h4>
+        <h4 className="font-semibold text-secondary-300">Recommendation car</h4>
         <MinimalButton label="View all" />
       </div>
       <div className="grid grid-cols-4 gap-8">
-        {carDesc.map((desc) => <CatalogCard key={uuidv4()} desc={desc} />).splice(0, 8)}
+        {carDesc.map((desc) => <CatalogCard key={uuidv4()} desc={desc} />).splice(0, count)}
       </div>
       <div className="relative flex items-center justify-center mt-16">
-        <PrimaryButton label="Show more car" />
+        {count <= carDesc.length && (
+          <PrimaryButton label="Show more car" onClick={() => handleShowMore()} />
+        )}
         <span className="absolute right-0 text-sm font-semibold text-secondary-300">
           {carDesc.length} car
         </span>
--- src/components/ui/PrimaryButton.jsx
+++ src/components/ui/PrimaryButton.jsx
@@ -1,10 +1,11 @@
 import clsx from 'clsx';
 
-export const PrimaryButton = ({ label, className, color }) => {
+export const PrimaryButton = ({ label, className, onClick, color }) => {
   const bgColor = color ? `bg-${color}` : 'bg-primary-500';
 
   return (
     <button
+      onClick={() => onClick()}
       className={clsx(
         'py-2.5 px-5 font-semibold text-white border-[1px] border-transparent rounded',
         className,
